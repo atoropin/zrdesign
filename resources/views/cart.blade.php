b4d7af1bed2bb35d65266089593f53deb941b748
atoropin <a.toropin@nekrasovka.ru>
2018-10-09 14:39:57 +0300

    Currency update

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -84,7 +84,7 @@ p.error-browser
                     </div>
                     <div class="content-product-info">
                         <div class="content-product-info-price"><span class="content-product-info-price-button">
-                                @isset($item->product->manufacturer->currency){{ strrev(chunk_split(strrev($item->product->base_price * $item->product->manufacturer->currency->exchange_rate), 3, ' ')) }} руб.@elseЦена по запросу@endisset
+                                @isset($item->product->manufacturer->currency){{ strrev(chunk_split(strrev($item->product->base_price * (int)$item->product->manufacturer->currency->exchange_rate), 3, ' ')) }} руб.@elseЦена по запросу@endisset
                             </span></div>
                         <div class="content-product-info-cart"><a class="content-product-info-cart-button" href='#'
                                                                           onclick="removeFromCart({{ $item->id }}); return false" style="text-decoration: none">Удалить</a>
@@ -93,7 +93,7 @@ p.error-browser
                 </div>
             @endforeach
             <div class="content-product-info">
-                <span class="content-product-info-price-button" id="totalPrice">Общая стоимость: {{ strrev(chunk_split(strrev($totalPrice), 3, ' ')) }} руб.</span>
+                <span class="content-product-info-price-button" id="totalPrice">Общая стоимость: {{ strrev(chunk_split(strrev((int)$totalPrice), 3, ' ')) }} руб.</span>
             </div>
             <div class="cart-form">
                 <form method="POST" action="/cart/send">
